a5d0e607ec6aa34c5707b121a21fff050e60b84c
Defacto0 <defactoroyale@gmail.com>
2025-06-03 15:14:26 +0300

    Homework classes

---
+++ Class/Class/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Unit
+{
+    public string Name { get; }
+    private float _health;
+    public float Health => _health;
+    public int Damage { get; } = 5;
+    public float Armor { get; }
+    public Unit() : this("Unknown Unit") { }
+    public Unit(string name)
+    {
+        Name = name;
+        Armor = 0.6f;
+    }
+    public float GetRealHealth()
+    {
+        return Health * (1f + Armor);
+    }
+    public bool SetDamage(float value)
+    {
+        _health -= value * Armor;
+        if (_health <= 0f)
+        {
+            return true; 
+        }
+        else
+        {
+            return false; 
+        }
+    }
+}
